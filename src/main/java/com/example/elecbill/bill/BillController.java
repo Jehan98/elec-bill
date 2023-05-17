9ea4d608b1e14878bd4f36c108217c1722b98b82
Jehan <jehansasindu98@gmail.com>
2023-05-17 19:52:39 +0530

    Add units to the apicall

--- src/main/java/com/example/elecbill/bill/BillController.java
+++ src/main/java/com/example/elecbill/bill/BillController.java
@@ -3,6 +3,7 @@ package com.example.elecbill.bill;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,7 +18,7 @@ public class BillController {
 
     @GetMapping
 	@CrossOrigin(origins = "http://localhost:4200")
-	public String getBill(){
+	public String getBill(@RequestParam("value") int requestData){
 		return billService.getBill();
 	}
 
