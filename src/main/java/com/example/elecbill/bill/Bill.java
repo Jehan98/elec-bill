69bb549eaa5c76788ffb47af0fccf5161490da19
Jehan <jehansasindu98@gmail.com>
2023-05-17 17:16:37 +0530

    Fix database not connecting

--- src/main/java/com/example/elecbill/bill/Bill.java
+++ src/main/java/com/example/elecbill/bill/Bill.java
@@ -1,12 +1,37 @@
 package com.example.elecbill.bill;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
 public class Bill{
+    @Id
+    @SequenceGenerator(
+        name = "bill_sequence",
+        sequenceName = "bill_sequence",
+        allocationSize = 1
+    )
+    @GeneratedValue(
+        strategy = GenerationType.SEQUENCE,
+        generator = "bill_sequence"
+    )
+    private Long id;
     private double fixed_cost;
     private double variable_cost;
 
-    public Bill(double fixed_cost, double variable_cost){
+    public Bill( Long id, double fixed_cost, double variable_cost){
         this.fixed_cost = fixed_cost;
         this.variable_cost = variable_cost;
+        this.id = id;
+    }
+
+    public Long getId(){
+        return id;
     }
 
     public double getFixedCost(){
@@ -17,11 +42,23 @@ public class Bill{
         return variable_cost;
     }
 
+    public void setId(Long id){
+        this.id = id;
+    }
+
     public void setFixedCost(double fixed_cost){
         this.fixed_cost = fixed_cost;
     }
 
     public void setVariableCost(double variable_cost){
         this.variable_cost = variable_cost;
+    }
+
+    public String toString(){
+        return "Bill{"+
+                "id=" + id +
+                ", fixed_cost=" + fixed_cost + '\'' +
+                ", variable_cost=" + variable_cost + 
+                '}';
     }
 }
